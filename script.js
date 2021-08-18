d27d6a269a29536a09d52825be544b829a6c6df1
iamskay <87765229+iamskay@users.noreply.github.com>
2021-08-18 15:42:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+console.log("Good Morning Skay")
+let panels=document.querySelectorAll('.panel');
+panels.forEach((panel) => {
+    panel.addEventListener('click', ()=>{
+        removeActiveClasses();
+        panel.classList.add('active');
+    });
+    
+});
+
+function removeActiveClasses(){
+    panels.forEach((panel) => {
+        panel.classList.remove('active')
+    })
+}
